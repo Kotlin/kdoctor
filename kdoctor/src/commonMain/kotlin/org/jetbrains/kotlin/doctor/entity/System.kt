5c745d56c2ca7709d96e904b319b9a7ec7cda6e4
Matsehor Artur <macegorarthur@gmail.com>
2022-02-21 13:26:36 +0200

    Rosetta detection and support added (#12)
    * Rosetta detection and support added
    
    * Rosetta 2 usage notification moved to a separate info message
    
    * Rosetta 2 usage notification moved to a separate info message
    
    Co-authored-by: arturm <arturm@hexagontech.co.il>

--- kdoctor/src/commonMain/kotlin/org/jetbrains/kotlin/doctor/entity/System.kt
+++ kdoctor/src/commonMain/kotlin/org/jetbrains/kotlin/doctor/entity/System.kt
@@ -19,13 +19,20 @@ object System {
     val type: SystemType = getCurrentSystemType()
     val homeDir: String = getHomeDir()
 
+    val isUsingRosetta by lazy { isUsingRosetta() }
+
     fun getVersion() =
         System.execute("sw_vers", "-productVersion").output?.let { Version(it) }
 
-    fun getHardwareInfo(): String? =
+    fun getHardwareInfo(): String? = if (isUsingRosetta) {
+        System.execute("sysctl", "-n", "machdep.cpu.brand_string", "").output
+            ?.let { "CPU: $it" }
+    } else {
         System.execute("system_profiler", "SPHardwareDataType").output?.lines()
             ?.firstOrNull { it.contains("Processor Name") || it.contains("Chip") }
-            ?.split(":")?.lastOrNull()?.let { "CPU: $it" }
+            ?.split(":")?.lastOrNull()
+            ?.let { "CPU: $it" }
+    }
 
     fun findAppPaths(appId: String): List<String> =
         System.execute("/usr/bin/mdfind", "kMDItemCFBundleIdentifier=\"$appId\"").output
@@ -40,6 +47,10 @@ object System {
         val shellPath = getEnvVar("SHELL")
         return Shell.values().singleOrNull { it.path == shellPath }
     }
+
+    private fun isUsingRosetta(): Boolean = System.execute("sysctl", "sysctl.proc_translated").output
+        ?.substringAfter("sysctl.proc_translated: ")
+        ?.toIntOrNull() == 1
 }
 
 expect fun System.getCurrentSystemType(): SystemType
