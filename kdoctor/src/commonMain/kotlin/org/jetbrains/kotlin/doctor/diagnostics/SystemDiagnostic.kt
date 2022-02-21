5578f289f21bb2b4f16c7a67a10bc32ba5542c9e
amatsehor <macegorarthur@gmail.com>
2022-02-21 10:52:25 +0200

    Rosetta 2 usage notification moved to a separate info message

--- kdoctor/src/commonMain/kotlin/org/jetbrains/kotlin/doctor/diagnostics/SystemDiagnostic.kt
+++ kdoctor/src/commonMain/kotlin/org/jetbrains/kotlin/doctor/diagnostics/SystemDiagnostic.kt
@@ -4,15 +4,24 @@ import org.jetbrains.kotlin.doctor.entity.System
 import org.jetbrains.kotlin.doctor.entity.SystemType
 
 class SystemDiagnostic : Diagnostic("System") {
-    override fun runChecks() = listOf(
-        Message(
-            if (System.type == SystemType.MacOS) ResultType.Success else ResultType.Failure,
-            """
-                OS: ${System.type} (${System.getVersion() ?: "N/A"})
-                ${System.getHardwareInfo() ?: ""}
-                Using Rosetta 2: ${if (System.isUsingRosetta) "Yes" else "No"}
-                
-            """.trimIndent()
+
+    override fun runChecks() = buildList {
+        add(
+            Message(
+                resultType = if (System.type == SystemType.MacOS) ResultType.Success else ResultType.Failure,
+                text = """
+                    OS: ${System.type} (${System.getVersion() ?: "N/A"})
+                    ${System.getHardwareInfo() ?: ""}
+                """.trimIndent()
+            )
         )
-    )
+
+        if (System.isUsingRosetta) {
+            this.addInfo(
+                title = "You are currently using Rosetta 2.",
+                "It may cause some issues while trying to install packages using Homebrew.",
+                "Consider switching off Rosetta 2 or ignore this message in case you actually need it."
+            )
+        }
+    }
 }
