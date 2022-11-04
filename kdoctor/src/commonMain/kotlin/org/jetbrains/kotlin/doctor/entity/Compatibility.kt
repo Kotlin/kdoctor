aaf7e5917ec29597aaf4b546628333d0de781497
konstantin.tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2022-11-04 17:01:48 +0100

    Add compatibility downloading.

--- kdoctor/src/commonMain/kotlin/org/jetbrains/kotlin/doctor/entity/Compatibility.kt
+++ kdoctor/src/commonMain/kotlin/org/jetbrains/kotlin/doctor/entity/Compatibility.kt
@@ -1,7 +1,12 @@
 package org.jetbrains.kotlin.doctor.entity
 
+import co.touchlab.kermit.Logger
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 
 @Serializable
 enum class EnvironmentPiece {
@@ -38,4 +43,19 @@ data class CompatibilityProblem(
 @Serializable
 data class Compatibility(
     @SerialName("problems") val problems: List<CompatibilityProblem>
-)
+) {
+    companion object {
+        suspend fun download() = try {
+            Logger.d("Compatibility.download")
+            //todo fix json url!
+            val compatibilityJson = System.httpClient.get(
+                "https://github.com/Kotlin/kdoctor/raw/wip/kdoctor/src/commonMain/resources/compatibility.json"
+            ).bodyAsText()
+            Logger.d(compatibilityJson)
+            Json.decodeFromString<Compatibility>(compatibilityJson)
+        } catch (e: Exception) {
+            Logger.e("Compatibility.download error ${e.message}", e)
+            Compatibility(emptyList())
+        }
+    }
+}
