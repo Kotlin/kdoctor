e9971987997874b3db459b3f2a5a13fd7e80e9e9
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2022-11-29 18:02:31 +0100

    Add CombinationsTest

---
+++ kdoctor/src/jvmTest/kotlin/org/jetbrains/kotlin/doctor/entity/CombinationsTest.kt
@@ -0,0 +1,59 @@
+package org.jetbrains.kotlin.doctor.entity
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class CombinationsTest {
+
+    @Test
+    fun `check all combinations`() {
+        val input = listOf(
+            listOf("@"),
+            listOf("a", "b"),
+            listOf("1", "2"),
+            listOf("#"),
+        )
+        val expected = listOf(
+            listOf("@", "a", "1", "#"),
+            listOf("@", "a", "2", "#"),
+            listOf("@", "b", "1", "#"),
+            listOf("@", "b", "2", "#"),
+        )
+        val actual = allCombinations(input)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `check all combinations with nulls`() {
+        val input = listOf(
+            listOf("@"),
+            listOf("a", "b"),
+            listOf(null),
+            listOf("1", "2"),
+            listOf("#"),
+        )
+        val expected = listOf(
+            listOf("@", "a", null, "1", "#"),
+            listOf("@", "a", null, "2", "#"),
+            listOf("@", "b", null, "1", "#"),
+            listOf("@", "b", null, "2", "#"),
+        )
+        val actual = allCombinations(input)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `check filter empty`() {
+        val input = listOf(
+            listOf("@"),
+            listOf(),
+            listOf("a", "b"),
+        )
+        val expected = listOf(
+            listOf("@", "a"),
+            listOf("@", "b"),
+        )
+        val actual = allCombinations(input)
+        assertEquals(expected, actual)
+    }
+}
