309f17c19e0f0f105cee991ad573cfa8b91c33bc
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2022-12-19 12:01:04 +0100

    Extract argument parsing

---
+++ kdoctor/src/commonMain/kotlin/org/jetbrains/kotlin/doctor/Arguments.kt
@@ -0,0 +1,39 @@
+package org.jetbrains.kotlin.doctor
+
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+import kotlinx.cli.default
+
+class Arguments(args: Array<String>) {
+    private val parser = ArgParser("kdoctor")
+
+    val isShowVersion: Boolean by parser.option(
+        ArgType.Boolean,
+        fullName = "version",
+        description = "print KDoctor version"
+    ).default(false)
+
+    val isVerbose: Boolean by parser.option(
+        ArgType.Boolean,
+        shortName = "v",
+        fullName = "verbose",
+        description = "print extended information"
+    ).default(false)
+
+    val isDebug: Boolean by parser.option(
+        ArgType.Boolean,
+        fullName = "debug",
+        description = "debug mode"
+    ).default(false)
+
+    val projectPath: String? by parser.option(
+        ArgType.String,
+        shortName = "p",
+        fullName = "project",
+        description = "path to a Gradle project root directory"
+    )
+
+    init {
+        parser.parse(args)
+    }
+}
--- kdoctor/src/commonMain/kotlin/org/jetbrains/kotlin/doctor/Main.kt
+++ kdoctor/src/commonMain/kotlin/org/jetbrains/kotlin/doctor/Main.kt
@@ -3,50 +3,25 @@ package org.jetbrains.kotlin.doctor
 import co.touchlab.kermit.CommonWriter
 import co.touchlab.kermit.Logger
 import co.touchlab.kermit.Severity
-import kotlinx.cli.ArgParser
-import kotlinx.cli.ArgType
 import kotlinx.coroutines.runBlocking
 import org.jetbrains.kotlin.doctor.entity.System
 
 internal const val KDOCTOR_VERSION = "0.0.6"
 
 fun main(args: Array<String>) {
-    val parser = ArgParser("kdoctor")
-    val versionFlag by parser.option(
-        ArgType.Boolean,
-        fullName = "version",
-        description = "print KDoctor version"
-    )
-    val verboseFlag by parser.option(
-        ArgType.Boolean,
-        shortName = "v",
-        fullName = "verbose",
-        description = "print extended information"
-    )
-    val projectPath by parser.option(
-        ArgType.String,
-        shortName = "p",
-        fullName = "project",
-        description = "path to a Gradle project root directory"
-    )
-    val debugFlag by parser.option(
-        ArgType.Boolean,
-        fullName = "debug",
-        description = "debug mode"
-    )
-    parser.parse(args)
+    val arguments = Arguments(args)
 
     Logger.setLogWriters(object : CommonWriter() {
-        override fun isLoggable(severity: Severity) = debugFlag == true
+        override fun isLoggable(severity: Severity) = arguments.isDebug
     })
 
-    val verbose = verboseFlag == true
     when {
-        versionFlag == true -> {
+        arguments.isShowVersion -> {
             println(KDOCTOR_VERSION)
         }
+
         else -> {
-            diagnoseKmmEnvironment(verbose, projectPath)
+            diagnoseKmmEnvironment(arguments.isVerbose, arguments.projectPath)
         }
     }
 }
