13510f535b443c1464b416f78cac75567ad3f6cf
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2023-01-02 16:02:07 +0100

    Update Kotlin to 1.8.0

--- kdoctor/build.gradle.kts
+++ kdoctor/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("multiplatform") version "1.7.20"
-    kotlin("plugin.serialization") version "1.7.20"
+    kotlin("multiplatform") version "1.8.0"
+    kotlin("plugin.serialization") version "1.8.0"
 }
 
 repositories {
