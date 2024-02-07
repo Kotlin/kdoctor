9159b0026d415737c5a0efb7a840a9853bed3837
Florian Kistner <florian.kistner@jetbrains.com>
2024-02-07 12:13:26 +0100

    Reengineer Doctor workflow to be reusable

--- kdoctor-checks/src/commonMain/kotlin/org/jetbrains/kotlin/doctor/Doctor.kt
+++ kdoctor-checks/src/commonMain/kotlin/org/jetbrains/kotlin/doctor/Doctor.kt
@@ -1,114 +1,107 @@
 package org.jetbrains.kotlin.doctor
 
 import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.ProducerScope
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.channels.SendChannel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
 import org.jetbrains.kotlin.doctor.compatibility.CompatibilityAnalyse
-import org.jetbrains.kotlin.doctor.diagnostics.*
+import org.jetbrains.kotlin.doctor.diagnostics.Diagnostic
 import org.jetbrains.kotlin.doctor.entity.*
 import org.jetbrains.kotlin.doctor.printer.TextPainter
 
 class Doctor(private val system: System) {
-    fun diagnoseKmmEnvironment(
+    fun diagnoseEnvironment(
         verbose: Boolean,
         debug: Boolean,
-        extraDiagnostics: Boolean,
+        diagnostics: Flow<Diagnostic>,
+        supplementalDiagnostics: Flow<Diagnostic>,
+        extraDiagnostics: Flow<Diagnostic>,
         localCompatibilityJson: String?,
-        templateProjectTag: String?
-    ): Flow<String> = channelFlow {
-        if (verbose) {
-            send("Environment diagnose:\n")
-        } else {
-            send("Environment diagnose (to see all details, use -v option):\n")
-        }
-
-        val compatibility = async {
-            if (localCompatibilityJson == null) {
-                Compatibility.download(system)
-            } else {
-                Compatibility.from(system, system.readFile(localCompatibilityJson).orEmpty())
-            }
-        }
-
-        suspend fun Diagnostic.run(): Diagnosis {
-            val progress = if (!debug) showDiagnosticProgress(title) else null
-            val diagnose = diagnose()
-            val text = "\r" + diagnose.getText(verbose) + if (verbose) "\n\n" else "\n"
-            progress?.cancel()
-            send(text)
-            return diagnose
-        }
-
-        val diagnosis = buildList<Diagnosis> {
-            add(SystemDiagnostic(system).run())
-            add(JavaDiagnostic(system).run())
-            add(AndroidStudioDiagnostic(system, true).run())
-            add(XcodeDiagnostic(system, true).run())
-
-            val mainEnvironmentIsNotReady = this.any { it.conclusion == DiagnosisResult.Failure }
-
-            add(CocoapodsDiagnostic(system).run())
-
-            if (extraDiagnostics) {
-                if (mainEnvironmentIsNotReady) {
-                    val msg = with(DiagnosisResult.Failure) {
-                        "$color[$symbol]${TextPainter.RESET} To run extra diagnostics setup your environment before."
-                    } + if (verbose) "\n\n" else "\n"
-                    send(msg)
+        output: SendChannel<String>
+    ): Flow<Conclusive> = channelFlow {
+        try {
+            val compatibility = async {
+                if (localCompatibilityJson == null) {
+                    Compatibility.download(system)
                 } else {
-                    if (templateProjectTag != null) {
-                        add(TemplateProjectDiagnostic(system, templateProjectTag).run())
-                    } else {
-                        add(TemplateProjectDiagnostic(system).run())
-                    }
-
-                    val path = system.pwd()
-                    add(GradleProjectDiagnostic(system, path).run())
+                    Compatibility.from(system, system.readFile(localCompatibilityJson).orEmpty())
                 }
             }
-        }
-        if (!verbose) {
-            send("\n")
-        }
 
-        val checkedEnvironments = diagnosis.map { it.checkedEnvironments }
-        system.logger.d("Environments: \n${checkedEnvironments.joinToString("\n")}")
+            fun CoroutineScope.showDiagnosticProgress(title: String) = launch {
+                var progressIndex = 0
+                while (isActive) {
+                    progressIndex = (progressIndex + 1) % progressSymbols.size
+                    val symbol = progressSymbols[progressIndex]
+                    output.send("\r[$symbol] $title")
+                    delay(60)
+                }
+            }
 
-        val allUserEnvironments = allCombinations(checkedEnvironments).map { combination ->
-            val environment = mutableSetOf<EnvironmentPiece>()
-            combination.forEach { environment.addAll(it) }
-            environment
-        }
+            suspend fun Diagnostic.run(): Diagnosis {
+                val progress = if (!debug) showDiagnosticProgress(title) else null
+                val diagnose = diagnose()
+                send(diagnose)
+                val text = "\r" + diagnose.getText(verbose) + if (verbose) "\n\n" else "\n"
+                progress?.cancel()
+                output.send(text)
+                return diagnose
+            }
 
-        val compatibilityReport =
-            CompatibilityAnalyse(system, compatibility.await(), true)
-                .check(allUserEnvironments, verbose).trim()
+            val checkedEnvironments = buildList {
+                var mainEnvironmentIsNotReady = false
 
-        if (compatibilityReport.isNotBlank()) {
-            send(compatibilityReport + "\n")
-        }
+                diagnostics.collect { diagnostic ->
+                    val diagnosis = diagnostic.run()
+                    if (diagnosis.conclusion == DiagnosisResult.Failure) mainEnvironmentIsNotReady = true
+                    add(diagnosis.checkedEnvironments)
+                }
 
-        send("Conclusion:\n")
+                supplementalDiagnostics.collect { diagnostic ->
+                    add(diagnostic.run().checkedEnvironments)
+                }
 
-        val hasFailures = diagnosis.any { it.conclusion == DiagnosisResult.Failure }
-        if (hasFailures) {
-            val prefix = "  ${DiagnosisResult.Failure.color}${DiagnosisResult.Failure.symbol}${TextPainter.RESET} "
-            send("${prefix}KDoctor has diagnosed one or more problems while checking your environment.\n")
-            send("    Please check the output for problem description and possible solutions.\n")
-        } else {
-            val prefix = "  ${DiagnosisResult.Success.color}${DiagnosisResult.Success.symbol}${TextPainter.RESET} "
-            send("${prefix}Your operation system is ready for Kotlin Multiplatform Mobile Development!\n")
-        }
-    }.buffer(0).flowOn(Dispatchers.Default)
+                var isFirstExtraDiagnostic = true
+                extraDiagnostics.collect { diagnostic ->
+                    when {
+                        mainEnvironmentIsNotReady -> when {
+                            isFirstExtraDiagnostic -> {
+                                isFirstExtraDiagnostic = false
+                                val msg = with(DiagnosisResult.Failure) {
+                                    "$color[$symbol]${TextPainter.RESET} To run extra diagnostics setup your environment before."
+                                } + if (verbose) "\n\n" else "\n"
+                                output.send(msg)
+                            }
+                        }
+                        else -> add(diagnostic.run().checkedEnvironments)
+                    }
+                }
+            }
 
-    private val progressSymbols = listOf("⣷", "⣯", "⣟", "⡿", "⢿", "⣻", "⣽", "⣾")
-    private fun ProducerScope<String>.showDiagnosticProgress(title: String) = launch {
-        var progressIndex = 0
-        while (isActive) {
-            progressIndex = (progressIndex + 1) % progressSymbols.size
-            val symbol = progressSymbols[progressIndex]
-            send("\r[$symbol] $title")
-            delay(60)
+            if (!verbose) {
+                output.send("\n")
+            }
+
+            system.logger.d("Environments: \n${checkedEnvironments.joinToString("\n")}")
+
+            val allUserEnvironments = allCombinations(checkedEnvironments).map { combination ->
+                val environment = mutableSetOf<EnvironmentPiece>()
+                combination.forEach { environment.addAll(it) }
+                environment
+            }
+
+            val compatibilityReport =
+                CompatibilityAnalyse(system, compatibility.await(), true)
+                    .check(allUserEnvironments, verbose)
+
+            send(compatibilityReport)
+            if (compatibilityReport.conclusion != DiagnosisResult.Success) {
+                output.send(compatibilityReport.text)
+            }
+        } finally {
+            output.close()
         }
     }
+
+    private val progressSymbols = listOf("⣷", "⣯", "⣟", "⡿", "⢿", "⣻", "⣽", "⣾")
 }
